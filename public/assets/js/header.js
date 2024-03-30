6e4cb63e81c841618052ff71a7ba409d6e47fd9a
mahatotarit <taritmahato1404@gmail.com>
2024-03-30 12:02:10 +0530

    side bar function completed

--- public/assets/js/header.js
+++ public/assets/js/header.js
@@ -1,5 +1,24 @@
 async function header(){
-//   alert('i am fine!');
 
-  document.querySelector('p').innerText = "all thing's are runnig properly";
+  let aside_parent_div = document.querySelector('#aside_parent');
+  // close side bar fun
+  let close_sidebar_btn = document.querySelector('.aside_close_button');
+  close_sidebar_btn.addEventListener('click',close_side_bar);
+  function close_side_bar(){
+    aside_parent_div.classList.remove('open_side_bar');
+  }
+
+  // open side bar fun
+  let open_side_bar_btn = document.querySelector('#open_side_bar_three_line');
+  open_side_bar_btn.addEventListener('click',open_side_bar);
+  function open_side_bar(){
+    aside_parent_div.classList.add('open_side_bar');
+  }
+
+  // close side bar when user click white space of aside_parent div
+  document.getElementById('aside_parent').addEventListener('click', function (event) {
+      if (event.target === this) { aside_parent_div.classList.remove('open_side_bar'); }
+  });
+
+  
 }
