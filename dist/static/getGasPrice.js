9fa7324eb9a8046593cf3df0394f93e72a991b73
mahatotarit <taritmahato1404@gmail.com>
2024-04-02 11:33:32 +0530

    gas tracker page design 99% completed

--- dist/static/getGasPrice.js
+++ dist/static/getGasPrice.js
@@ -1,5 +1,27 @@
 const axios = require('axios');
 
+const networks = [
+  ['Arbitrum Mainnet', '42161'],
+  ['Arbitrum Nova', '42170'],
+  ['Avalanche Mainnet', '43114'],
+  ['Base Mainnet', '8453'],
+  ['BNB Chain Mainnet', '56'],
+  ['opBNB (layer 2)', '204'],
+  ['Cronos Mainnet', '25'],
+  ['Ethereum Mainnet', '1'],
+  ['Ethereum Goerli', '5'],
+  ['Ethereum Sepolia', '11155111'],
+  ['Fantom Mainnet', '250'],
+  ['Filecoin Mainnet', '314'],
+  ['Linea Mainnet', '59144'],
+  ['Linea Testnet', '59140'],
+  ['Optimism Mainnet', '10'],
+  ['Polygon Mainnet', '137'],
+  ['Polygon Mumbai', '80001'],
+  ['zkSync Era Mainnet', '324'],
+];
+
+
 const Auth = Buffer.from('13d9b7d0ef994b09840355a7ed0e406a' +':' +'K+LmU7ijS9Gc7ac6oQBcWl8cAIrkxdhaP5ew5q4YjeVO+/xCacoUiw',).toString('base64');
 require('events').EventEmitter.defaultMaxListeners = 1000;
 
@@ -12,3 +34,4 @@ async function getGasPrice(network_id) {
     console.log('Server responded with:', error);
   }
 }
+console.log(getGasPrice(networks[5][1]))
